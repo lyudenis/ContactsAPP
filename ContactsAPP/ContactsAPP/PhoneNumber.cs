acd29f132ef9119c4759d2bf9a404079f26f6bff
lyudenis <lyudenis@yandex.ru>
2024-09-21 19:05:33 +0300

    добавление классов

---
+++ ContactsAPP/ContactsAPP/PhoneNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp
+{
+    public class PhoneNumber
+    {
+        private long _phone;
+        /// <summary>
+        /// номер телефона содержит 11 цифр и начинается с 7
+        /// </summary>
+        public long Number
+        {
+            get { return _phone; }
+
+            set
+            {
+
+                if (value < 10000000000 || value > 99999999999)
+                {
+                    throw new ArgumentException("Номер должен состоять из 11 цифр.");
+                }
+                if (value < 70000000000 || value > 79999999999)
+                {
+                    throw new ArgumentException("Номер должен начинаться с цифры 7.");
+                }
+                else { _phone = value; }
+
+            }
+
+        }
+    }
+}
---
+++ ContactsAPP/ContactsAPP/Project.cs
@@ -0,0 +1,37 @@
+﻿using ContactsAPP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp
+{
+    public class Project
+    {
+        /// <summary>
+        /// Список контактов
+        /// </summary>
+        public List<Contact> _contactslistone = new List<Contact>();
+        public List<Contact> Sort(List<Contact> _contactslistone)
+        {
+            _contactslistone.Sort(delegate (Contact x, Contact y)
+            {
+                return x.Sername.CompareTo(y.Sername);
+            });
+            return _contactslistone;
+        }
+        public static Project Birthday(Project data, DateTime today)
+        {
+            Project returnProject = new Project();
+            for (int i = 0; i < data._contactslistone.Count; i++)
+            {
+                if ((data._contactslistone[i].Birth.Day == today.Day) && (data._contactslistone[i].Birth.Month == today.Month))
+                {
+                    returnProject._contactslistone.Add(data._contactslistone[i]);
+                }
+            }
+            return returnProject;
+        }
+    }
+}
